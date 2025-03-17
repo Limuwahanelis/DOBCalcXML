fe1d0b3a345199bf203fd609b3e7d31dc5a0d416
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-17 23:42:48 +0100

    Set today date as starting date

--- app/src/main/java/com/example/dobcalcxml/MainActivity.kt
+++ app/src/main/java/com/example/dobcalcxml/MainActivity.kt
@@ -14,6 +14,10 @@ import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private var tvSelectedDate :TextView?=null;
+    private val myCalendar:Calendar = Calendar.getInstance();
+    private val year:Int=myCalendar.get(Calendar.YEAR);
+    private val month:Int = myCalendar.get(Calendar.MONTH);
+    private val day:Int = myCalendar.get(Calendar.DAY_OF_MONTH)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,15 +25,16 @@ class MainActivity : AppCompatActivity() {
         val btnDatePicker:Button=findViewById(R.id.buttonDatePicker)
         tvSelectedDate = findViewById(R.id.tvSelectedDate);
 
+        val selectedDate ="$day/${month+1}/$year";
+        tvSelectedDate?.text = selectedDate;
+
+
         btnDatePicker.setOnClickListener { clickDatePicker()}
     }
 
     fun clickDatePicker()
     {
-        val myCalendar:Calendar = Calendar.getInstance();
-        val year:Int=myCalendar.get(Calendar.YEAR);
-        val month:Int = myCalendar.get(Calendar.MONTH);
-        val day:Int = myCalendar.get(Calendar.DAY_OF_MONTH)
+
         DatePickerDialog(this,
             DatePickerDialog.OnDateSetListener { view,year,month,dayOfMonth->
                 Toast.makeText(this,"Date picker works",Toast.LENGTH_LONG).show();
@@ -38,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
                 val sdf:SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
                 val theDate:Date? = sdf.parse(selectedDate);
-                
+
             },
             year,
             month,
