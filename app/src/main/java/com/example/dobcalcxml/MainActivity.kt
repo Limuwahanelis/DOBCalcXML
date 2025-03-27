5061d3326ec5ffb62446bf70a1a8f95944d8366b
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-27 13:12:42 +0100

    Implement saving state and selected date in date dialog. Add separate horizontal xml.

--- app/src/main/java/com/example/dobcalcxml/MainActivity.kt
+++ app/src/main/java/com/example/dobcalcxml/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -17,9 +18,9 @@ class MainActivity : AppCompatActivity() {
     private var tvSelectedDate :TextView?=null;
     private var tvAgeInMinutes :TextView?=null;
     private val myCalendar:Calendar = Calendar.getInstance();
-    private val year:Int=myCalendar.get(Calendar.YEAR);
-    private val month:Int = myCalendar.get(Calendar.MONTH);
-    private val day:Int = myCalendar.get(Calendar.DAY_OF_MONTH)
+    private var year:Int=myCalendar.get(Calendar.YEAR);
+    private var month:Int = myCalendar.get(Calendar.MONTH);
+    private var day:Int = myCalendar.get(Calendar.DAY_OF_MONTH)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,16 +31,33 @@ class MainActivity : AppCompatActivity() {
 
         val selectedDate ="$day/${month+1}/$year";
         tvSelectedDate?.text = selectedDate;
-
-
+        val dd:List<Int> = mutableListOf(1,2,3);
+        //dd[0]=3;
         btnDatePicker.setOnClickListener { clickDatePicker()}
     }
+    @Override
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray("DATE", intArrayOf(day,month,year));
+        val selectedDate ="$day/${month+1}/$year";
+        outState.putString("DATES",tvSelectedDate?.text.toString());
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+            day= savedInstanceState.getIntArray("DATE")!![0];
+            month= savedInstanceState.getIntArray("DATE")!![1];
+            year= savedInstanceState.getIntArray("DATE")!![2];
+            tvSelectedDate?.text = savedInstanceState.getString("DATES");
+    }
     private fun clickDatePicker()
     {
 
         val dpd = DatePickerDialog(this,
             DatePickerDialog.OnDateSetListener { _,year,month,dayOfMonth->
+                day=dayOfMonth;
+                this.month = month;
+                this.year=year;
                 val selectedDate ="$dayOfMonth/${month+1}/$year";
                 tvSelectedDate?.text = selectedDate;
 
